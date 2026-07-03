e46a7e553eb4750a4b80e576dc06de2f849bf3e1
vgxmallu <DeltaAlgor@outlook.com>
2026-07-03 22:47:10 +0530

    Delete ultis directory

--- ultis/helper.py
+++
@@ -1,9 +0,0 @@
-import traceback
-
-async def safe_task(coro):
-
-    try:
-        await coro
-
-    except Exception:
-        traceback.print_exc()
--- ultis/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-from config import LOG_CHANNEL 
-
-logging.basicConfig(
-    level=getattr(logging, LOG_CHANNEL),
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-)
-
-logger = logging.getLogger("VGX")
