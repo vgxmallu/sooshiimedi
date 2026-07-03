2d61b7850eff3660e06b2526de71b37247285d2e
vgxmallu <DeltaAlgor@outlook.com>
2026-07-03 23:38:43 +0530

    VGX ⚡

--- mbot/plugins/Fdl.py
+++ mbot/plugins/Fdl.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import yt_dlp
-from config import COOKIES_FILE, DOWNLOAD_DIR
+from config import COOKIES_FILE, DOWNLOAD_DIR, LOG_CHANNEL
 
 from pyrogram import Client, filters
 from pyrogram.types import Message 
@@ -40,6 +40,21 @@ async def download_facebook_video(url: str, download_dir: str = "downloads"):
     return await loop.run_in_executor(None, _download_sync, url, download_dir)
 
 
+FB = """
+🔵⬜ **FACEBOOK LINK ALERT** 🔵⬜
+
+👊➖➖➖➖➖➖➖➖➖👊
+📛**FACEBOOK link** : [click here]({})
+👤**Name** : {}
+👾**Username** : @{}
+💾**DC** : {}
+♐**ID** : `{}`
+🤖**BOT** : @SocialMediaX_dlbot
+➖➖➖➖➖➖➖➖➖➖
+
+#facebook #fb
+"""
+
 
 # Regex specifically targeting Facebook, FB Watch, and Reels variants
 FB_REGEX = r"(https?://)?(www\.|m\.|watch\.|web\.)?(facebook\.com|fb\.watch|fb\.gg|fb\.com)/.+"
@@ -50,20 +65,30 @@ async def handle_facebook_links(client: Client, message: Message):
     url = message.matches[0].group(0)
     
     status = await message.reply_text("🔎 **Link detected. Processing...**")
-    
+    user = message.from_user
+    mention = user.mention if user else "Anonymous"
+    username = f"@{user.username}" if user and user.username else "No Username"
+    dc_id = user.dc_id if user else "Unknown"
+    user_id = user.id if user else message.chat.id
+
+# Send the log message using the safe variables
+    gg = await client.send_message(
+        LOG_CHANNEL, 
+        IG.format(url, mention, username, dc_id, user_id)
+    )
     try:
-        await status.edit_text("📥 **Downloading video...**")
+        await status.edit_text("📥 **Downloading Facebook Video...**")
         file_path, title = await download_facebook_video(url, DOWNLOAD_DIR)
         
         if not os.path.exists(file_path):
             await status.edit_text("❌ Download failed: Media path not found.")
             return
             
-        await status.edit_text("📤 **Uploading file...**")
+        await status.edit_text("📤 **Uploading To Telegram...**")
         
         await message.reply_video(
             video=file_path,
-            caption=f"🎬 **{title}**"
+            caption=f"🎬 **{title}**\n\n©️ @SocialMediaX_dlbot\n🔥🤖 @XBOTS_X"
         )
         
         if os.path.exists(file_path):
