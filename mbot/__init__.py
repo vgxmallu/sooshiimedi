86f9f8dfbdba8b8ad7f708e0ffc87de48fff7f31
vgxmallu <DeltaAlgor@outlook.com>
2026-07-03 22:42:34 +0530

    VGX ⚡

--- mbot/__init__.py
+++ mbot/__init__.py
@@ -5,19 +5,13 @@ from vgx_config import Config
 
 
 
-# Setup clean logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger("mbot")
 
-# Initialize the bot
 app = Client(
-    name="mbot_session",
+    "VGXBot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="mbot/plugins"), # Uses dot-notation for safer importing
-    workers=16
+    workers=100,
+    sleep_threshold=30,
+    plugins=dict(root="mbot.plugins")
 )
