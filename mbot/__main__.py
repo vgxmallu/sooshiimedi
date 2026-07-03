8425bc602952b504ce89791159015308e25bc765
vgxmallu <DeltaAlgor@outlook.com>
2026-07-03 20:48:02 +0530

    VGX ⚡

--- mbot/__main__.py
+++ mbot/__main__.py
@@ -1,12 +1,9 @@
 from pyrogram import idle
 from mbot import app
 
-async def main():
-    await app.start()
-    print("🟢🟢BOT IS RUNNING!🟢🟢")
-    await idle()
-    await app.stop()
-
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    app.start()
+
+    print("🚀 Bot Started! Send /schedule")
+    idle()
+    app.stop()
